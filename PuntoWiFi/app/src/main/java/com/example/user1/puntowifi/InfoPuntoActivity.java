63f358829184a6fc6717423726c5f377e727d51e
Sebastián Vallejo Roldán <sebastianvallejoroldan@gmail.com>
2018-06-01 04:04:35 -0500

    botones funcionando y API de Google Maps

--- PuntoWiFi/app/src/main/java/com/example/user1/puntowifi/InfoPuntoActivity.java
+++ PuntoWiFi/app/src/main/java/com/example/user1/puntowifi/InfoPuntoActivity.java
@@ -1,14 +1,28 @@
 package com.example.user1.puntowifi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class InfoPuntoActivity extends AppCompatActivity {
-
+    Button ubicarPunto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_punto);
+
+        ubicarPunto=(Button)findViewById(R.id.btnUbicarPunto);
+
+        ubicarPunto.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Intent intent =new Intent(InfoPuntoActivity.this,MapsActivity.class);
+                startActivity(intent);
+            }
+
+        });
 
     }
 }

368287526df81c0809432fcf04fd8e27d4833a1a
Sebastián Vallejo Roldán <sebastianvallejoroldan@gmail.com>
2018-06-01 00:54:12 -0500

    xd

--- PuntoWiFi/app/src/main/java/com/example/user1/puntowifi/CalcularPuntosActivity.java
+++ PuntoWiFi/app/src/main/java/com/example/user1/puntowifi/CalcularPuntosActivity.java
@@ -2,6 +2,8 @@ package com.example.user1.puntowifi;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class CalcularPuntosActivity extends AppCompatActivity {
 
--- PuntoWiFi/app/src/main/java/com/example/user1/puntowifi/LoginActivity.java
+++ PuntoWiFi/app/src/main/java/com/example/user1/puntowifi/LoginActivity.java
@@ -6,15 +6,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
 
+    // Connect to your database.
+    // Replace server name, username, and password with your credentials
+    public static void main(String[] args) {
+        String connectionString =
+                "jdbc:sqlserver://14.0.1000.169:1433;"
+                        + "database=Puntos WiFi;"
+                        + "user=yourusername@yourserver;"
+                        + "password=yourpassword;"
+                        + "encrypt=true;"
+                        + "trustServerCertificate=false;"
+                        + "hostNameInCertificate=*.database.windows.net;"
+                        + "loginTimeout=30;";
 
+        // Declare the JDBC objects.
+        Connection connection = null;
 
+        try {
+            connection = DriverManager.getConnection(connectionString);
 
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (connection != null) try { connection.close(); } catch(Exception e) {}
+        }
     }
 }
